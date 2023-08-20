176b21443f1fdae8fcfe4a1229c82d81f8520248
pa4op <artyrik-6@mail.ru>
2023-08-21 00:16:38 +0300

    first commit: add all files

---
+++ CursorTeleport.cs
@@ -0,0 +1,38 @@
+﻿
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace CursorTeleport
+{
+   
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+
+        private ModConfig Config;
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        
+        public override void Entry(IModHelper helper)
+        {
+            this.Config = helper.ReadConfig<ModConfig>();
+            helper.Events.Input.ButtonsChanged += this.OnButtonsChanged;
+        }
+
+        private void OnButtonsChanged(object sender, ButtonsChangedEventArgs e)
+        {
+            if (this.Config.ToggleKey.JustPressed())
+            {
+                var x = e.Cursor.AbsolutePixels.X;
+                var y = e.Cursor.AbsolutePixels.Y;
+                Game1.player.position.X = x - 32;
+                Game1.player.position.Y = y;
+            }
+        }
+    }
+}
---
+++ ModConfig.cs
@@ -0,0 +1,11 @@
+﻿
+using StardewModdingAPI.Utilities;
+
+
+namespace CursorTeleport
+{
+    internal class ModConfig
+    {
+        public KeybindList ToggleKey { get; set; } = KeybindList.Parse("V");
+    }
+}
